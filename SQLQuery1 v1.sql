3ebd7f7f2a1ea739eb0e185f2b69c259c63fc4c8
anands1714 <92007695+anands1714@users.noreply.github.com>
2021-10-06 19:29:34 -0400

    Update SQLQuery1 v1.sql

--- SQLQuery1 v1.sql
+++ SQLQuery1 v1.sql
@@ -22,7 +22,7 @@ order by 1,2
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..['Covid deaths$']
-Where location like '%states%'
+Where location like '%canada%'
 and continent is not null 
 order by 1,2
 
@@ -32,7 +32,6 @@ order by 1,2
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..['Covid deaths$']
---Where location like '%states%'
 order by 1,2
 
 
@@ -40,7 +39,6 @@ order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..['Covid deaths$']
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -49,7 +47,6 @@ order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..['Covid deaths$']
---Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -62,7 +59,6 @@ order by TotalDeathCount desc
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..['Covid deaths$']
---Where location like '%states%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -73,9 +69,7 @@ order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..['Covid deaths$']
---Where location like '%states%'
 where continent is not null 
---Group By date
 order by 1,2
 
 
@@ -85,7 +79,6 @@ order by 1,2
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
 From PortfolioProject..['Covid deaths$'] dea
 Join PortfolioProject..['Covid vaccinations$'] vac
 	On dea.location = vac.location
@@ -101,13 +94,13 @@ as
 (
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+ 
 From PortfolioProject..['Covid deaths$'] dea
 Join PortfolioProject..['Covid vaccinations$'] vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
---order by 2,3
+ 
 )
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
